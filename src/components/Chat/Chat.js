889dab60226598323d7b38913f73e2dd29376721
Nidhi Dad <dadnidhi@gmail.com>
2020-08-16 18:58:58 +0530

    Updated endpoint to heroku

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://live-chat-client.herokuapp.com/';
+  const ENDPOINT = 'https://livechat-server-piyush.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
